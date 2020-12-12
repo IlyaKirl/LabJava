ddc04ad2312b3677c5cf972028ff4e7ca94cb3cc
IlyaKirl <ascs16@mail.ru>
2020-12-12 12:20:15 +0300

    lab1

--- src/main/java/Exc/TestSpring.java
+++ src/main/java/Exc/TestSpring.java
@@ -1,6 +1,5 @@
 package Exc;
 
-import TransferQuestion.TrQuestion;
 import TransferQuestion.TreatmentQuestion;
 import com.opencsv.exceptions.CsvException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,11 +8,8 @@ import java.io.IOException;
 
 public class TestSpring {
     public static void main(String[] args) throws IOException, CsvException {
-
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TrQuestion trQuestion = context.getBean("TrQuestion", TrQuestion.class);
-        TreatmentQuestion testAssemble = context.getBean("TreatmentQuestion", TreatmentQuestion.class);
-        testAssemble.TestAssemble(trQuestion.getListQuestion());
-        context.close();
+        TreatmentQuestion TestRun = context.getBean(TreatmentQuestion.class);
+        TestRun.run();
     }
 }
